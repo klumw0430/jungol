d77e06f163c8b085280778042f008b7c0500299a
klumw <klumw0430@gmail.com>
2022-02-19 10:01:00 +0900

    fix bug 615.c

--- 615.c
+++ 615.c
@@ -11,8 +11,8 @@ int main()
   struct student s2;
   scanf("%s %d %d", s1.name, &s1.score[0], &s1.score[1]);
   scanf("%s %d %d", s2.name, &s2.score[0], &s2.score[1]);
-  int avg[2] = {s1.score[0] + s1.score[1] / 2,
-    s2.score[0] + s2.score[1] / 2};
+  int avg[2] = {(s1.score[0] + s2.score[0]) / 2,
+    (s1.score[1] + s2.score[1]) / 2};
   printf("%s %d %d\n%s %d %d\navg %d %d",
     s1.name, s1.score[0], s1.score[1],
     s2.name, s2.score[0], s2.score[1],
