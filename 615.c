244937b93220d3cde6615bf37023b2fb97adc6be
klumw <klumw0430@gmail.com>
2022-02-19 10:11:18 +0900

    add avg function

--- 615.c
+++ 615.c
@@ -5,17 +5,22 @@ struct student {
   int score[2];
 };
 
+int avg(int x, int y)
+{
+  int avg = (x + y) / 2;
+  return avg;
+}
+
 int main()
 {
   struct student s1;
   struct student s2;
   scanf("%s %d %d", s1.name, &s1.score[0], &s1.score[1]);
   scanf("%s %d %d", s2.name, &s2.score[0], &s2.score[1]);
-  int avg[2] = {(s1.score[0] + s2.score[0]) / 2,
-    (s1.score[1] + s2.score[1]) / 2};
-  printf("%s %d %d\n%s %d %d\navg %d %d",
+  printf("%s %d %d\n%s %d %d\navg %d %d\n",
     s1.name, s1.score[0], s1.score[1],
     s2.name, s2.score[0], s2.score[1],
-    avg[0], avg[1]);
+    avg(s1.score[0], s2.score[0]),
+    avg(s1.score[1], s2.score[1]));
   return 0;
 }
