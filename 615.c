598fc6c5108a92a39b9793b92a5fb374f835386d
klumw <klumw0430@gmail.com>
2022-02-19 10:43:09 +0900

    add scanf fuction 615.c

--- 615.c
+++ 615.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-struct student {
+typedef struct student {
   char name[20];
   int score[2];
-};
+} student;
 
 int avg(int x, int y)
 {
@@ -11,12 +11,17 @@ int avg(int x, int y)
   return avg;
 }
 
+void get(student *s)
+{
+  scanf("%s %d %d", s->name, &s->score[0], &s->score[1]);
+}
+
 int main()
 {
-  struct student s1;
-  struct student s2;
-  scanf("%s %d %d", s1.name, &s1.score[0], &s1.score[1]);
-  scanf("%s %d %d", s2.name, &s2.score[0], &s2.score[1]);
+  student s1;
+  student s2;
+  get(&s1);
+  get(&s2);
   printf("%s %d %d\n%s %d %d\navg %d %d\n",
     s1.name, s1.score[0], s1.score[1],
     s2.name, s2.score[0], s2.score[1],
